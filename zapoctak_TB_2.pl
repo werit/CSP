b771557ca7ebf16988684cabfa4d9a14d700ad12
Werit_HP_Spectre <tib.balaz@gmail.com>
2018-02-22 03:02:22 +0100

    added predicates for bind schedule

--- zapoctak_TB_2.pl
+++ zapoctak_TB_2.pl
@@ -70,7 +70,10 @@ prepareRoomSchedule(Days, Res, [Sch|Schedule]) :-
 %    Sum #= Sch + TmpSum.
 
 
-% prepReser // prepareReservations 
+% prepReser // prepareReservations
+% predicate goes over all reservations and call updateRoomBasedOnRes,
+% which updates every room based on reservation preperties
+
 % prepReser(+Number, +Reservations, +Rooms, +Schedule,
 %                     -TotalPaid, -Vals).
 prepReser(_,[],-,_,0,[]).
@@ -80,8 +83,47 @@ prepReser(Number,[Res1|ReservaRest],Rooms,SchIn,Paid,Vals):-
   prepReser(Num1,ReservaRest,Rooms,SchIn,PaidRest,ValsSub).
 
 % updateRoomBasedOnRes(+Number,+Res1,+Rooms,+SchIn,-PayPerRoom)
-updateRoomBasedOnRes(Number,Res1,[Room|RoomsRest],SchIn,PayPerRoom):-
+updateRoomBasedOnRes(Number,([Start,End|_],Days,People,MaxPrice),[Room|RoomsRest],[Sch|SchRest],PayPerRoom):-
+updateSchedule(Number,Start,End,Room,People,Sch), % take one room and its schedule and bind it
+updateRoomBasedOnRes(Number,([Start,End|_],Days,People,MaxPrice),RoomsRest,SchRest,PayPerRoom).
 
+% getRightDayInRoom predicate iterates over days till it is equal of first possible day of reservation
+% getRightDayInRoom(+Number,+Start,+Room,+People,+Sch)
+updateSchedule(Number,Start,End,Room,People,Sch):-
+  Room#>=People,!, % this room is suitable for the people from reservation
+  Day=1,
+updateBasedOnDay(Day,Number,Start,End,Sch).
+
+  updateSchedule(Number,Start,End,Room,People,Sch):-
+    Room#<People,!, % this room is NOT suitable for the people from reservation
+updateBasedOnDayRemoveAll(Number,Sch). % This could be also done with updateBasedOnDay predicate with Start and End parameter being f.e. 2 and 1 respectively
+
+% predicate updates schedules
+% when the day is not the day that reservation lasts (between its possible start and end)
+% then this reservation's number is removed from schedule of this day
+% updateBasedOnDay(+Day,+Number,+Start,+End,+Sch)
+updateBasedOnDay(_,_,_,_,[]).
+updateBasedOnDay(Day,Number,Start,End,[Sch|SchRest]):-
+  Day >=Start,
+  Day =< End,!, % we are in possible day for reservation
+  DayNext is Day + 1; % so we do nothing and move on to the next day
+  updateBasedOnDay(DayNext,Number,Start,End,SchRest).
+
+% here we remove reservation's number from schedule where it does not belong
+updateBasedOnDay(Day,Number,Start,End,[Sch|SchRest]):-
+  % this could be included but it is not necessary because of previous predicate with cut.
+  % (Day <Start;Day > End),
+  Sch#\=Number, % on this day reservation is not posssible because the reservation is not happening during this day.
+  DayNext is Day + 1;
+  updateBasedOnDay(DayNext,Number,Start,End,SchRest).
+
+% Room is not suitable for reservation and we are romoving it.
+% Reservation is represented by number and we are removing this number.
+% updateBasedOnDayRemoveAll(+Number,+Sch).
+updateBasedOnDayRemoveAll(_,[]).
+updateBasedOnDayRemoveAll(Number,[Sch|SchRest]):-
+Sch#\=Number,
+updateBasedOnDayRemoveAll(Number,SchRest).
 
 % printSchedule(Schedule).
 %
